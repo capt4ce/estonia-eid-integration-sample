5e219482cd10b919b028f53e23b834b2d594db1a
Ahmad Ali Abdilah <ahmad.ali.abdilah@gmail.com>
2021-11-12 20:43:04 +0800

    restoring past work

---
+++ README.md
@@ -0,0 +1,4 @@
+## estonia-eid-integration-sample
+
+This is a sample project to integrate Estonia eID to a webpage.
+This project is the only thing recovered from the time I was working on it, so it's not so straight forward to start this project. I will update it in the future to make starting this project straight forwar \*finger-crossed
---
+++ reactComponentSample.ts
@@ -0,0 +1,148 @@
+// Licensed to the Quasisoft Limited - Hong Kong under one or more agreements
+// The Quasisoft Limited - Hong Kong licenses this file to you under MIT license.
+
+import React, { useState } from 'react';
+import { sha3_256 } from 'js-sha3';
+
+import { SendTxBuilder, EstoniaEid } from '../../../';
+
+const SendTxBuilder = ({ sender, recipient, fee, amount }) => {
+  // mock txBuilder returning Tx bytes
+  return new Uint8Array(100);
+};
+
+const bytesToHexes = (byteArr) => {
+  const a = [];
+  byteArr.forEach((item) => {
+    const data = item.toString(16);
+    data.length === 1 ? a.push(`0${data}`) : a.push(data);
+  });
+  return a.join('');
+};
+
+function hexToBytes(hex) {
+  for (var bytes = [], c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
+}
+
+const SignSendTxWithEid = () => {
+  const estoniaEidObj = new EstoniaEid();
+  const [eidPublicKey, setEidPublicKey] = useState('');
+  const [recipient, setRecipient] = useState('');
+  const [amount, setAmount] = useState('');
+  const [fee, setFee] = useState('');
+  const [signature, setSignature] = useState('');
+  const [txBytesAndSignature, setTxBytesAndSignature] = useState('');
+
+  const resetDisplay = (isKeepPublicKey) => {
+    if (!isKeepPublicKey) {
+      setEidPublicKey('');
+    }
+    setSignature('');
+  };
+
+  const handleChangeRecipient = ({ target: { value } = {} } = {}) => {
+    setRecipient(value);
+    resetDisplay(true);
+  };
+
+  const handleChangeAmount = ({ target: { value } = {} } = {}) => {
+    setAmount(value);
+    resetDisplay(true);
+  };
+
+  const handleChangeFee = ({ target: { value } = {} } = {}) => {
+    setFee(value);
+    resetDisplay(true);
+  };
+
+  const buildTransaction = () => {
+    const txBytes = SendTxBuilder({
+      sender: { value: hexToBytes(eidPublicKey), type: 3 },
+      recipient: { value: recipient, type: 0 },
+      fee: parseInt(fee),
+      amount: parseInt(amount),
+    });
+    return txBytes;
+  };
+
+  const getPublicKeyFromEid = async () => {
+    try {
+      const { publicKey } = await estoniaEidObj.getCardInfo();
+      setEidPublicKey(bytesToHexes(publicKey));
+    } catch (e) {
+      console.error('getPublicKeyFromEid', e);
+    }
+  };
+
+  const signWithEid = async () => {
+    try {
+      const txBytes = buildTransaction();
+      const txBytesString = bytesToHexes(txBytes);
+
+      const txHash = await sha3_256(txBytes);
+
+      const { signature } = await estoniaEidObj.signData(txHash);
+      const signatureString = signature;
+
+      setSignature(signatureString);
+      setTxBytesAndSignature(txBytesString + signatureString);
+    } catch (e) {
+      console.error('signWithEid', e);
+    }
+  };
+
+  return (
+    <div>
+      <div style={{ textAlign: 'center' }}>
+        <div>
+          <button onClick={getPublicKeyFromEid}>Get Public Key from eID</button>
+        </div>
+        <div>
+          <h4>eID Public Key:</h4>
+          <textarea
+            style={{ width: '800px', textAlign: 'center' }}
+            value={eidPublicKey || '--'}
+          />
+        </div>
+        <div>
+          <h4>Recipient:</h4>
+          <input
+            name="recipient"
+            onChange={handleChangeRecipient}
+            value={recipient}
+          />
+        </div>
+        <div>
+          <h4>Amount:</h4>
+          <input name="amount" onChange={handleChangeAmount} value={amount} />
+        </div>
+        <div>
+          <h4>Fee:</h4>
+          <input name="fee" onChange={handleChangeFee} value={fee} />
+        </div>
+        <div>
+          <button onClick={signWithEid}>Sign with eID</button>
+        </div>
+        <div>
+          <h4>Signature:</h4>
+          <textarea
+            style={{ width: '800px', textAlign: 'center' }}
+            value={signature || '--'}
+          />
+        </div>
+        <div>
+          <h4>TxBytes + signature:</h4>
+          <textarea
+            rows={10}
+            style={{ width: '800px', textAlign: 'center' }}
+            value={txBytesAndSignature || '--'}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignSendTxWithEid;
